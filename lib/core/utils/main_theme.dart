8f1fd2f7c878efe282128314f419350249571d24
SadullaevTolkinjon <sadullayevtolik@gmail.com>
2022-05-08 13:58:47 +0500

    files added

---
+++ lib/core/utils/main_theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MainTheme {
+  static const Color _whiteColor = Color(0xffFFFFFF);
+  static const Color _blackColor = Color(0xff242424);
+
+  static ThemeData light = ThemeData(
+    // ? FontFamily
+
+    fontFamily: GoogleFonts.inter().fontFamily,
+
+    // ? Elevated Buttons Style
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        primary: const Color(0XFF7BA3F2),
+        textStyle: const TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.w600,
+          color: _whiteColor,
+        ),
+      ),
+    ),
+
+    // ? Appbars Style
+
+    appBarTheme: const AppBarTheme(
+        foregroundColor: _blackColor,
+        backgroundColor: _whiteColor,
+        elevation: 2,
+        titleTextStyle: TextStyle(
+            fontSize: 18, fontWeight: FontWeight.w600, color: _blackColor)),
+
+    // ? Scaffold background Color
+
+    scaffoldBackgroundColor: _whiteColor,
+
+    // ? Floating action Buttons styles
+
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: Color(0XFF7BA3F2),
+      unselectedItemColor: Colors.white,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+    ),
+  );
+}
---
+++ lib/core/utils/routes.dart
@@ -0,0 +1,15 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:humo_test_app/views/home/view/home_page_view.dart';
+
+class RouterGenerator {
+  static onGenerateRoutes(RouteSettings settings) {
+    var args = settings.arguments;
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(builder: (context) => const HomePage());
+    }
+  }
+}
